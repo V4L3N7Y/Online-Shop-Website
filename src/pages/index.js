af6fbb0acd5b91bcae4eef8179c5e75d5c0806e9
Nastase Valentin <65254776+V4L3N7Y@users.noreply.github.com>
2023-01-09 13:58:49 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home({products}) {
   return (
     <div className="bg-gray-100">
       <Head>
-        <title>Amazon</title>
+        <title>oNLINEsHOP</title>
       </Head>
 
          <Header/>
